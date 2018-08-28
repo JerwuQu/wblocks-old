45d16c9812416dbb00a6a17db71a558e3e2f69c5
Marcus Ramse <marcus@ramse.se>
2018-08-28 07:39:18 +0200

    Default color in config

--- wblocks/wblocks.c
+++ wblocks/wblocks.c
@@ -16,10 +16,12 @@ wchar_t* strWiden(const char* inStr, int inLen, int* outLen)
     return outStr;
 }
 
+// Reads a string from config table
+// Returns string pointer on success, NULL on fail
 static char* toml_table_string(toml_table_t* table, char* key)
 {
-    const char* raw;
-    if (raw = toml_raw_in(table, key)) {
+    const char* raw = toml_raw_in(table, key);
+    if (raw) {
         char* str;
         if (toml_rtos(raw, &str)) {
             printf("Failed to parse 'wblocks.toml': Invalid string '%s'\n", key);
@@ -28,6 +30,39 @@ static char* toml_table_string(toml_table_t* table, char* key)
         return str;
     } else {
         return NULL;
+    }
+}
+
+static inline int toml_array_int(toml_array_t* colorarr, int idx, int64_t* out)
+{
+    const char* raw;
+    if (!(raw = toml_raw_at(colorarr, idx))) {
+        printf("Failed to parse 'wblocks.toml': Invalid array index %d\n", idx);
+        return 1;
+    }
+
+    if (toml_rtoi(raw, out)) {
+        printf("Failed to parse 'wblocks.toml': Invalid integer at array index %d\n", idx);
+        return 1;
+    }
+
+    return 0;
+}
+
+// Reads a color array from config table
+// Returns 0 on success, 1 on fail
+static int toml_table_color(toml_table_t* table, char* key, COLORREF* out)
+{
+    toml_array_t* colorarr = toml_array_in(table, key);
+    if (colorarr) {
+        int64_t r, g, b;
+        if (toml_array_int(colorarr, 0, &r)) return 1;
+        if (toml_array_int(colorarr, 1, &g)) return 1;
+        if (toml_array_int(colorarr, 2, &b)) return 1;
+        *out = RGB(r & 0xFF, g & 0xFF, b & 0xFF);
+        return 0;
+    } else {
+        return 1;
     }
 }
 
@@ -48,6 +83,8 @@ static int loadBlock(toml_table_t* block)
         style.minWidthStr.wstr = strWiden(str, (int)strlen(str), &style.minWidthStr.wlen);
         free(str);
     }
+
+    toml_table_color(block, "color", &style.color);
 
     // Finally, create block using text source
     if (str = toml_table_string(block, "script")) {
